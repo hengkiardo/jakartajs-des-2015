3648b04460cf2e20c602459f9253286ddbf68b56
Hengkiardo <hengki.sihombing@gmail.com>
2015-12-16 14:04:45 +0700

    parsing data form pages requested

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ var fs       = require('fs')
 var Request   = require('request')
 var async     = require('async')
 var _         = require('lodash')
+var cheerio   = require('cheerio')
 
 async.waterfall([
-  requestPage
+  requestPage,
+  cheerioScrape
 ], function (err, result) {
-  if (err) console.log(err)
-  // console.log(result)
+
+  if (err) {
+    console.log(err)
+  }
+
+  console.log(result)
+
 })
 
 function requestPage(callback) {
+
   var options = {
     url: 'http://www.lazada.co.id/beli-handphone-tablet/',
     headers: {
@@ -21,7 +29,46 @@ function requestPage(callback) {
   };
 
   Request(options, function (err, response, body) {
-    console.log(err, _.result(response, 'statusCode'))
+    // console.log(err, _.result(response, 'statusCode'))
     callback(null, body)
+  })
+}
+
+function cheerioScrape(html, callback) {
+
+  var $ = cheerio.load(html)
+
+  var productCards = []
+
+  var parsingProducts = function(card, cb) {
+
+    var singleProduct = {
+      name : $(card).find('.product-card__name-wrap span').attr('title'),
+
+      photo : $(card).find('.product-card__img img').attr('data-original'),
+
+      product_link : $(card).find('> a').attr('href'),
+
+      price_normal : $(card).find('.old-price-wrap .product-card__old-price').text(),
+
+      price_discount : $(card).find('.old-price-wrap .product-card__sale').text(),
+
+      sale_number : $(card).find('.product-card__price').text(),
+
+      isCredit : $(card).hasClass('IP'),
+
+      rating : {
+        number : $(card).find('.rating__number').text(),
+        star : $(card).find('.icon-rating_stars_disabled').attr('title')
+      }
+    }
+
+    productCards.push(singleProduct)
+
+    cb()
+  };
+
+  async.each( $('.component-product_list .product-card') , parsingProducts, function (err) {
+    callback(null, productCards)
   })
 }
